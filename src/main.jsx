af7596174293c3f2d94e1d28479f35f2dce8d54e
codinglone <codinglone@gmail.com>
2023-06-09 18:18:10 +0400

    Added react-responsive carousel

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import { ThemeProvider } from "@material-tailwind/react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
