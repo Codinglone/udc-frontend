1b6e0554138b3fac2eab763e05ab5940d00fe84b
codinglone <codinglone@gmail.com>
2023-06-09 11:46:43 +0400

    Changed the logo picture to png and added basic navbar boilerplate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,27 @@
+import logo from "../assets/logo.png"
+
 const Navbar = () => {
   return (
-    <nav>
-        <span>Navbar component</span>
+    <nav className="shadow-lg">
+        <div className="flex">
+            <div id="brand" className="w-[5%]">
+                <img src={logo} className="object-contain" alt="" />
+            </div>
+            <div id="details" className="flex">
+                <div className="details-item">
+                <h3>Opening Hour</h3>
+                <span>Mon - Fri, 8:00 - 17:00</span>
+                </div>
+                <div className="details-item">
+                <h3>Call Us</h3>
+                <span>+250 784 427 142</span>
+                </div>
+                <div className="details-item">
+                <h3>Email Us</h3>
+                <span>codinglone@gmail.com</span>
+                </div>
+            </div>
+        </div>
     </nav>
   )
 }
