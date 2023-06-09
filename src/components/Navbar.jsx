8f534c4a8f30b522ed5c4fae3c5931e02d2780d0
codinglone <codinglone@gmail.com>
2023-06-09 12:04:04 +0400

    Added details part of navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,22 +3,23 @@ import logo from "../assets/logo.png"
 const Navbar = () => {
   return (
     <nav className="shadow-lg">
-        <div className="flex">
-            <div id="brand" className="w-[5%]">
-                <img src={logo} className="object-contain" alt="" />
+        <div className="flex justify-between md:mx-8 md:my-2">
+            <div id="brand" className="w-[40%] flex cursor-pointer">
+                <img src={logo} className="object-contain md:max-w-[10%]" alt="" />
+                <i className="font-semibold text-blue-900 text-2xl py-6">UDC LTD</i>
             </div>
-            <div id="details" className="flex">
+            <div id="details" className="flex justify-between py-4 md:w-[60%]">
                 <div className="details-item">
-                <h3>Opening Hour</h3>
-                <span>Mon - Fri, 8:00 - 17:00</span>
+                <h3 className="font-semibold text-blue-900">Opening Hour</h3>
+                <span className="font-semibold text-blue-900">Mon - Fri, 8:00 - 17:00</span>
                 </div>
                 <div className="details-item">
-                <h3>Call Us</h3>
-                <span>+250 784 427 142</span>
+                <h3 className="font-semibold text-blue-900">Call Us</h3>
+                <span className="font-semibold text-blue-900">+250 784 427 142</span>
                 </div>
                 <div className="details-item">
-                <h3>Email Us</h3>
-                <span>codinglone@gmail.com</span>
+                <h3 className="font-semibold text-blue-900">Email Us</h3>
+                <span className="font-semibold text-blue-900">codinglone@gmail.com</span>
                 </div>
             </div>
         </div>
