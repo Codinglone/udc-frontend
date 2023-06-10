27087a9aa17141ad9715fb6b411e9ab80b2223d0
codinglone <codinglone@gmail.com>
2023-06-10 21:24:22 +0400

    Added react countup functionality with static numbers

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -1,10 +1,35 @@
+import CountUp from "react-countup";
+
 const Status = () => {
+  return (
+    <>
+    <div>
+      <div>
+        <div>
+        <CountUp end={20} duration={5} />
+        EXPERT ENGINEERS & PROJECT MANAGERS
+        </div>
+        <div>
+        <CountUp end={35} duration={5} />
+        HAPPY CLIENTS
+        </div>
+      </div>
+     
+      <div>
+        <div>
+        <CountUp end={15} duration={5} />
+        COMPLETED PROJECTS
+        </div>
+        <div>
+        <CountUp end={20} duration={5} />
+        RUNNING PROJECTS
+        </div>
+      </div>
+     
+      
+      </div>
+    </>
+  );
+};
 
-    return (
-        <>
-            <h1>Status Component</h1>
-        </>
-    )
-}
-
-export default Status
+export default Status;
