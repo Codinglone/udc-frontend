4f36c2e60f612755eb47edf0b3ba60bb4a9ae597
codinglone <codinglone@gmail.com>
2023-06-10 20:11:28 +0400

    Added Description part data

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -2,7 +2,7 @@ import carousel3 from "../assets/carousel-3.jpg";
 
 const Description = () => {
   return (
-    <div className="my-8 mx-4 w-full flex ">
+    <div className="my-24 mx-4 w-full flex ">
       <div className="w-1/2">
         <img src={carousel3} alt="Photo" />
       </div>
@@ -12,17 +12,18 @@ const Description = () => {
           20 Years Of Experience
         </h1>
         <p className="my-2 font-bold text-blue-900">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
-          asperiores perspiciatis ex. Consequatur quam dolor beatae numquam
-          similique est obcaecati ipsam earum perferendis distinctio. Quae
-          itaque quas sint quo voluptas, molestias voluptatibus adipisci quia
-          doloribus cupiditate harum quaerat alias perspiciatis provident ab
-          excepturi quidem impedit, quisquam iure natus ullam illum sit culpa
-          sunt. Consectetur fuga culpa obcaecati, totam velit reprehenderit
-          blanditiis sed laborum eum laudantium tempore, cum quisquam quibusdam!
-          Magnam beatae aut consequatur dolores reprehenderit, ratione
-          asperiores aspernatur consectetur, autem quo, a cupiditate saepe ipsum
-          doloremque odio voluptatem quisquam error aliquid molestias facere
+          At Universal Design and Construction Ltd, we specialize in creating
+          innovative and transformative spaces that meet the unique needs of our
+          clients. As a leading construction company, we pride ourselves on
+          delivering exceptional craftsmanship, outstanding quality, and
+          unparalleled customer service. With a team of highly skilled
+          professionals, we offer a comprehensive range of construction
+          services, catering to both residential and commercial projects. From
+          concept to completion, we ensure meticulous attention to detail,
+          utilizing the latest industry techniques and materials to bring your
+          vision to life. Our commitment to excellence extends beyond just
+          building structures; we prioritize the principles of universal design
+          in all our projects. 
         </p>
       </div>
     </div>
