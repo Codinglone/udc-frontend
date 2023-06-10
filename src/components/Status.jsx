4f2012ee34025b02ba7447012aedc7c698fa56aa
codinglone <codinglone@gmail.com>
2023-06-10 21:44:57 +0400

    Added first part data in status component

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -4,18 +4,19 @@ const Status = () => {
   return (
     <>
     <div className="w-full flex">
-      <div className="w-1/2 bg-blue-900">
+      <div className="w-1/2 bg-blue-900 flex justify-between px-4 py-8">
         <div>
-        <CountUp end={20} duration={5} />
-        EXPERT ENGINEERS & PROJECT MANAGERS
+        <CountUp className="text-white text-6xl font-bold" end={20} duration={5} /> <br />
+       <span className="font-semibold text-lg text-yellow-300"> EXPERT ENGINEERS & <br /> PROJECT MANAGERS</span>
         </div>
         <div>
-        <CountUp end={35} duration={5} />
-        HAPPY CLIENTS
+        <CountUp className="text-white text-6xl font-bold" end={35} duration={5} /> <br />
+       <span className="font-semibold text-lg text-yellow-300">HAPPY CLIENTS</span>
         </div>
+      
       </div>
      
-      <div className="w-1/2 bg-[gold]">
+      <div className="w-1/2 bg-yellow-300">
         <div>
         <CountUp end={15} duration={5} />
         COMPLETED PROJECTS
