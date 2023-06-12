f82a7efec775f887a27882cc767ed264978caff6
codinglone <codinglone@gmail.com>
2023-06-12 11:36:04 +0400

    Added section title div in Description Component

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,18 +1,26 @@
 import carousel3 from "../assets/carousel-3.jpg";
-import AOS from "aos"
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 AOS.init();
 const Description = () => {
   return (
-    <div className="my-24 mx-4 w-full flex" data-aos="fade-up" data-aos-duration={1500}>
+    <div
+      className="my-24 mx-4 w-full flex"
+      data-aos="fade-up"
+      data-aos-duration={1500}
+    >
       <div className="w-1/2">
         <img src={carousel3} alt="Photo" />
       </div>
       <div className="w-[44%] ml-8" id="about">
-        <h3 className="text-blue-900 font-bold text-xl mb-2">Welcome to UDC</h3>
-        <h1 className="text-5xl font-bold text-blue-700">
-          20 Years Of Experience
-        </h1>
+        <div className="section-title">
+          <h3 className="text-blue-900 font-bold text-xl mb-2">
+            Welcome to UDC
+          </h3>
+          <h1 className="text-5xl font-bold text-blue-700">
+            20 Years Of Experience
+          </h1>
+        </div>
         <p className="my-2 font-bold text-blue-900">
           At Universal Design and Construction Ltd, we specialize in creating
           innovative and transformative spaces that meet the unique needs of our
@@ -25,7 +33,7 @@ const Description = () => {
           utilizing the latest industry techniques and materials to bring your
           vision to life. Our commitment to excellence extends beyond just
           building structures; we prioritize the principles of universal design
-          in all our projects. 
+          in all our projects.
         </p>
       </div>
     </div>
