1ffc135d32d4ad3ca53995720316581bab27a773
codinglone <codinglone@gmail.com>
2023-06-13 12:09:30 +0400

    Created Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import Description from "./components/Description"
 import Status from "./components/Status"
 import Services from "./components/Services"
 import ServicesTab from "./components/ServicesTab"
+import Footer from "./components/Footer"
 function App() {
 
   return (
+    <>
     <main className=" overflow-hidden">
     <Navbar />
     <Carousel />
@@ -15,6 +17,8 @@ function App() {
      <Services />
      <ServicesTab />
     </main>
+    <Footer />
+    </>
   )
 }
 
