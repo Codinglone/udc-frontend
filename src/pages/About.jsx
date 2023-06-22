43e5fe4c748ebc7c63b86ea7d2c5cfdb705d7a48
codinglone <codinglone@gmail.com>
2023-06-22 21:14:09 +0400

    Created pages for links

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Description from '../components/Description'
+
+const About = () => {
+  return (
+    <>
+        <h1>About Us</h1>
+        <Description />
+    </>
+    
+  )
+}
+
+export default About
