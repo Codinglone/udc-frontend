b0e7322d67dc25e5a3f17c0338ce5c28febb79a9
mutonirachel <109520133+mutonirachel@users.noreply.github.com>
2023-06-27 19:50:47 +0200

    seventh commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,40 +3,65 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="shadow-lg md:pb-8">
-        {/* <div className="flex justify-between md:mx-8 md:my-2">
-       
-            <div id="details" className="flex justify-between py-4 md:w-[60%]">
-                <div className="details-item">
-                <h3 className="font-semibold text-blue-900 cursor-pointer">Opening Hour</h3>
-                <span className="font-semibold text-blue-900 cursor-pointer">Mon - Fri, 8:00 - 17:00</span>
-                </div>
-                <div className="details-item">
-                <h3 className="font-semibold text-blue-900 cursor-pointer">Call Us</h3>
-                <span className="font-semibold text-blue-900 cursor-pointer">+250 784 427 142</span>
-                </div>
-                <div className="details-item">
-                <h3 className="font-semibold text-blue-900 cursor-pointer">Email Us</h3>
-                <span className="font-semibold text-blue-900 cursor-pointer">codinglone@gmail.com</span>
-                </div>
-            </div>
-        </div> */}
-        <div className="mav-menu bg-blue-900 p-6 flex items-center justify-between">
-            <div className="menus flex gap-12 px-80 py-7">
-            <div id="brand" className="absolute top-0 left-1 w-[40%] flex cursor-pointer">
-                <img src={logo} className="object-contain md:max-w-[20%] bg-wite" alt="" />
-                <i className="font-semibold text-white text-sm py-12">UNIVERSAL DESIGN AND <br /> CONSTRUCTION (UDC) LTD</i>
-            </div>
-                <a href="/home" className="active font-semibold">HOME</a>
-                <a href="/projects" id="ABOUTUS" className="font-semibold text-white hover:text-[gold]">PROJECTS</a>
-                <a href="/projects" className="font-semibold text-white hover:text-[gold]">PROJECTS</a>
-                <a href="/services" className="font-semibold text-white hover:text-[gold]">SERVICES</a>
-                <a href="/team" className="font-semibold text-white hover:text-[gold]">OUR TEAM</a>
-                {/* <a href="#" className=" px-2 font-semibold text-white hover:text-[gold]">CAREERS</a> */}
-                <a href="/contacts" className="font-semibold text-white hover:text-[gold]">CONTACT</a>
-            </div>
-             <button className="border-2 border-white p-2 text-white rounded-md font-semibold hover:text-[gold] hover:border-b-2 hover:border-[gold]">LOGIN</button>
+    <nav className="bg-gradient-to-r from-indigo-50 shadow-lg md:pb-1 w-full">
+      <div className="nav-menu p-6 flex items-center w-full">
+        <div className="menus flex w-full py-">
+          <div
+            id="brand"
+            className="absolute mb-4 top-1 left-0 w-1/3 flex cursor-pointer"
+          >
+            <img
+              src={logo}
+              className="object-contain mb-10 md:max-w-[20%] bg-wite"
+              alt=""
+            />
+            <i className="font-extrabold text-blue-700 text-sm py-7">
+              UNIVERSAL DESIGN AND <br /> CONSTRUCTION (UDC) LTD
+            </i>
+          </div>
+          <div className="flex gap-16 justify-between lg:ml-96">
+            <Link to="/" className="active text-blue-700 hover:text-[gold] font-bold">
+              HOME
+            </Link>
+            <Link 
+              to="/about"
+              id="ABOUTUS"
+              className="font-bold text-blue-700  hover:text-[gold]"
+            >
+              ABOUT US
+            </Link>
+            <Link
+              to="/projects"
+              className="font-bold text-blue-700  hover:text-[gold]"
+            >
+              PROJECTS
+            </Link>
+            <Link
+              to="/services"
+              className="font-bold text-blue-700  hover:text-[gold]"
+            >
+              SERVICES
+            </Link>
+            <Link
+              to="/team"
+              className="font-bold text-blue-700  hover:text-[gold]"
+            >
+              OUR TEAM
+            </Link>
+            <Link
+              to="/contacts"
+              className="font-bold text-blue-700  hover:text-[gold]"
+            >
+              CONTACT
+            </Link>
+          </div>
         </div>
+        <div className="">
+          <button className="border-2 border-blue-200 p-2 text-blue-700  rounded-md font-bold hover:text-[gold] hover:border-b-2 hover:border-[gold]">
+            LOGIN
+          </button>
+        </div>
+      </div>
     </nav>
   );
 };
