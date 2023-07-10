6a9092156dfee91480735a390df727d2c7f83ae1
Fabrice <108425198+Codinglone@users.noreply.github.com>
2023-07-11 01:27:17 +0400

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,10 +3,19 @@ import Gallery from "../components/Gallery";
 
 function Projects() {
   return (
-    <div className="mt-40">
+    <>
+             <div className='grid justify-center items-center'>
+        <div className="bg-gray-800 absolute z-50 opacity-50 w-full h-[40vh]"></div>
+        <img
+          src="src/assets/project3.jpg"
+          className="max-h-[40vh] object-cover min-w-[98vw]"
+        />
+       
+      </div>
+
       <Portfolio />
       <Gallery />
-    </div>
+    </>
   );
 }
 
