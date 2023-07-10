a7fc36db51db287123da4a4ed2b92fe5c4edbd92
Fabrice <108425198+Codinglone@users.noreply.github.com>
2023-07-11 01:22:34 +0400

    Update Description.jsx

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,23 +1,23 @@
-import aboutusImg from "../assets/aboutuspage.jpg";
+import carousel3 from "../assets/carousel-3.jpg";
 import SectionTitle from "./SectionTitle";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import CarouselComponent from "./Carousel";
+
 AOS.init();
 const Description = () => {
   return (
     <div
-      className="my-16 mx-6 flex"
+      className="my-6 mx-6 flex"
       data-aos="fade-up"
-      data-aos-duration={1500}
+      data-aos-duration={2000}
     >
-      <div className="w-1/2">
-        <img src={aboutusImg} alt="Photo" 
-        className="rounded-sm m-6" />
-      </div>
-      <div className="w-[44%] ml-16 my-3" id="about">
+  
+      <CarouselComponent />
+      <div className="w-[44%] ml-16 my-7" id="about">
 
         <SectionTitle title="" titleText="WELCOME TO UDC!" />
-        <p className="my-5 leading-7 text-justify text-neutral-900 text-lg">
+        <p className="my-5 leading-loose font-bold text-xl">
         At Universal Design And Construction, Ltd., we understand that
         each client is unique and has distinct dreams and aspirations.
         We prioritize client collaboration and aim to create spaces that 
@@ -27,31 +27,9 @@ const Description = () => {
         With a team of highly skilled architects, engineers, and construction
         professionals, Universal Design And Construction Ltd, brings together
         a wealth of expertise and experience to create stunning, functional, and
-        sustainable structures. Our team possesses a deep understanding of modern
-        architectural trends, incorporating luxurious aesthetics and green design
-        principles into every project. Universal Design And Construction, Ltd. is 
-        proud to have established a strong reputation for excellence, integrity, and
-        customer satisfaction. 
-          {/* At Universal Design and Construction Ltd, we specialize in creating
-          innovative and transformative spaces that meet the unique needs of our
-          clients. <br /> <i className="text-xl text-blue-800">MOTIVATION:</i> dedicated to creating exceptional spaces 
-          that inspire and exceed expectations. With a focus on collaboration, innovation, and 
-          sustainability, we turn dreams into reality, delivering outstanding residential, commercial, 
-          and hospitality projects. Join us on this exciting journey where luxury design meets sustainability.
-           Welcome to Universal Design And Construction, Ltd.!
-          <br /><i className="text-xl text-blue-800">OUR GOALS:</i> Client Satisfaction, Excellence in Design and Construction, Sustainable Practices, Timely Project
-           Delivery and Continuous Improvement.
-          <br /><i className="text-xl text-blue-800">OUR VALUES:</i> Client-Centric Approach, Integrity and Transparency, Collaboration and Teamwork, Innovation and Creativity, */}
-           {/* Sustainability and Environmental Responsibility and Quality and Craftsmanship. */}
-          {/* <br /><i className="text-xl text-blue-800"> OUR MISSION IS:</i> to create exceptional spaces that embody
-         our clients' dreams through modern design, luxury, and 
-         sustainable practices. We prioritize collaboration, 
-         innovation, and quality to deliver outstanding residential,
-         commercial, and hospitality projects that exceed expectations.
-         <br /><i className="text-xl text-blue-800">OUR VISION IS:</i> To be the leading provider of luxury design and construction 
-        services, integrating client aspirations, innovative architectural
-        concepts, and sustainable practices to create exceptional spaces 
-        that inspire and exceed expectations. */}
+        sustainabvle structures. By engaging in open and transparent 
+        communication, we ensure that our clients' needs are at the forefront
+        of every decision.
         </p>
       </div>
     </div>
