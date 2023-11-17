cae981c33cae94f9aa59e783dedb65c77279aa40
Codinglone <codinglone@gmail.com>
2023-11-17 18:21:19 +0200

    Made services responsive

--- src/components/NewServices.jsx
+++ src/components/NewServices.jsx
@@ -8,28 +8,28 @@ AOS.init();
 const NewServices = ({imgDir, title,content}) => {
     
   return (
-    <section className="px-24 py-24" id='services' data-aos="fade-up"
+    <section className="lg:px-24 px-16 py-24" id='services' data-aos="fade-up"
     data-aos-duration={2000}>
 
         {imgDir === 'right' ? (
-      <div className="flex justify-between">
+      <div className="flex lg:flex-row flex-col lg:justify-between">
         
-        <div className="flex flex-col w-[45%]">
-          <h1 className="text-4xl text-blue-700 font-bold uppercase">{title}</h1>
+        <div className="flex flex-col lg:w-[45%] w-full">
+          <h1 className="lg:text-4xl text-2xl text-blue-700 font-bold uppercase">{title}</h1>
           <p className="text-gray-900 text-justify pt-4 text-xl">{content}</p>
         </div>
-        <div className="w-[45%] border-4 border-blue-700 h-[49vh]">
+        <div className="lg:w-[45%] w-full border-4 border-blue-700 h-[23vh] h-[49vh]">
           <img src={project7} className="ml-[-40px] mt-[-40px] object-cover" />
         </div>
       </div>
       ) : 
 
-      <div className="flex justify-between">
-        <div className="w-[45%] border-4 border-blue-700 h-[49vh]">
-          <img src={project} className="ml-[-40px] mt-[-40px] object-cover" />
+      <div className="flex lg:flex-row flex-col lg:justify-between">
+        <div className="lg:w-[45%] w-full border-4 border-blue-700 h-[23vh] lg:h-[49vh]">
+          <img src={project} className="lg:ml-[-40px] ml-[-20px] mt-[-40px] object-cover" />
         </div>
-        <div className="flex flex-col w-[45%]">
-          <h1 className="text-4xl text-blue-700 font-bold uppercase">{title}</h1>
+        <div className="flex flex-col lg:w-[45%] w-full">
+          <h1 className="lg:text-4xl text-2xl my-8 lg:mt-0 text-blue-700 font-bold uppercase">{title}</h1>
           <p className="text-gray-900 text-justify pt-4 text-xl">{content}</p>
         </div>
       </div>
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -9,18 +9,18 @@ const ServicesPage = () => {
   return (
     <> 
             <div  
-            className='mt-28'
+            className='mt-28 lg:h-[34vh] h-12'
           style={{
             backgroundImage: `url(${project7})`,
             backgroundSize: 'cover',
             backgroundRepeat: 'no-repeat',
             backgroundPosition: 'center',
-            height: '34vh',
             width: '100%',
+            paddingTop: '1rem'
           
           }}
         >
-          <h1 className='lg:text-7xl text-white text-center font-bold lg:pl-24 lg:py-28 z-20'>Services</h1>
+          <h1 className='lg:text-7xl text-white text-center text-2xl font-bold lg:pl-24 z-20'>Services</h1>
         </div>
       <NewServices imgDir={'left'} title={'Civil Engineering'} content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, eligendi. Voluptatibus neque temporibus maxime, soluta nihil, ibusdam iste ipsum voluptates ut deleniti facilis! Tenetur maiores nisi possimus et facere perferendis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quidem impedit, veniam nihil quisquam aliquam temporibus! Quas ratione consectetur molestias nulla asperiores perferendis reiciendis. Iste tempore dolores suscipit culpa pariatur.'}/>
       <NewServices imgDir={'right'} title={'Land Surveying'} content={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, eligendi. Voluptatibus neque temporibus maxime, soluta nihil, ibusdam iste ipsum voluptates ut deleniti facilis! Tenetur maiores nisi possimus et facere perferendis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quidem impedit, veniam nihil quisquam aliquam temporibus! Quas ratione consectetur molestias nulla asperiores perferendis reiciendis. Iste tempore dolores suscipit culpa pariatur.'}/>
